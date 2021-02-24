9bf9f1249ee11d53b6924e488bdb9260bf5c6e87
My VO <kathy.nguyen198@gmail.com>
2021-02-24 12:53:20 +0100

    supprimer les commentaires

--- skeletonAPI-2/repositories/user.js
+++ skeletonAPI-2/repositories/user.js
@@ -1,5 +1,3 @@
-// const bcrypt = require('bcrypt');
-
 module.exports = (models) => {
 
     const user_repository = {
@@ -7,9 +5,6 @@ module.exports = (models) => {
             return models.user.query("select * from Users");
         },
         register: async (data) => {
-            // const password = data[1];
-            // const hashedPassword = await bcrypt.hash(password, 10)
-
             return models.user.query(`INSERT INTO Users(email, password) VALUES (?, ?)`, data)
         },
         getById: async (id) => {
